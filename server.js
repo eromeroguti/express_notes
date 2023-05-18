159221588a789a0fa3fa3345c45c79c644a4bbaf
eromeroguti <eromeroguti@gmail.com>
2023-05-17 19:13:40 -0700

    updated lines 23 thru 31

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(express.static('public'));
 
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
